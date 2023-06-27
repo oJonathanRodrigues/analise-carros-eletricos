1be71c328e2d0d00e809f2cd73d37f74ad5436f9
oJonathanRodrigues <118636531+oJonathanRodrigues@users.noreply.github.com>
2023-06-27 18:50:03 -0300

    Alteração no título

--- src/pageComponents/apresentacao/Titulo.tsx
+++ src/pageComponents/apresentacao/Titulo.tsx
@@ -3,7 +3,7 @@ export default function Slogan() {
     return (
         <div className="flex flex-col justify-center gap-5 bg-white pt-10">
             <div className="flex gap-2.5 text-4xl lg:text-4xl text-black font-bold p-3">
-            Carros elétricos: um olhar sobre o mercado global e nacional, as diferentes opções e o custo-benefício
+            Carros Elétricos: um olhar sobre o mercado global e nacional, as diferentes opções e o custo-benefício
             </div>
         </div>
     )
