e18e070a0780eab52ed1c355c951503603f22144
oJonathanRodrigues <118636531+oJonathanRodrigues@users.noreply.github.com>
2023-06-28 18:52:04 -0300

    Atualização do logo

--- src/pageComponents/comum/Logo.tsx
+++ src/pageComponents/comum/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
     return (
         <div className="flex flex-col justify-center items-center">
             <span className="font-black text-white text-3xl ">Jonathan</span>
-            <span className="text-zinc-100 font-thin text-2xl">Avaliação - carros elétricos</span>
+            <span className="text-zinc-100 font-thin text-2xl">Avaliação - Carros Elétricos</span>
         </div>
     )
 }
